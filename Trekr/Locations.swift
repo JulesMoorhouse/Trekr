f5b6856ecb824b991abc39e92fb644fd737e6440
Julian Moorhouse <julesthecoder@gmail.com>
2020-12-22 18:42:10 +0000

    Some progress.

---
+++ Trekr/Locations.swift
@@ -0,0 +1,12 @@
+//
+//  Locations.swift
+//  Trekr
+//
+//  Created by Julian Moorhouse on 22/12/2020.
+//
+
+import Foundation
+
+class Locations: ObservableObject {
+    let places: [Location]
+}
