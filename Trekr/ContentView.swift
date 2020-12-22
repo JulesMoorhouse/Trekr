c355cd9f94eab49ad8b19f747897ddb2c559aba9
Julian Moorhouse <julesthecoder@gmail.com>
2020-12-22 17:41:11 +0000

    Added initial files.

---
+++ Trekr/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Trekr
+//
+//  Created by Julian Moorhouse on 22/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("highlands")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
